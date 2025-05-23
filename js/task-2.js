d6db5db0a8d0c9a6d309798eb2363d738a514087
xxLEITTOxx <xxleittoxx@gmail.com>
2025-05-23 20:23:06 +0200

    job done

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,39 @@
 console.log("task 2");
-let country;
-let price;
-let deliveryFee;
 
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
+// makeArray, яка приймає три параметри:
+// firstArray(масив),
+//   secondArray(масив) і
+// maxLength(число).
 
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+function makeArray(firstArray, secondArray, maxLength) {
+  // Повертає новий масив, в якому перші елементи з
+  // firstArray, а потім з secondArray. Якщо загальна
+  // кількість елементів перевищує maxLength, то
+  // повертається новий масив з першими maxLength
+  // елементами.
+  const newArray = [...firstArray, ...secondArray];
+  return newArray.slice(0, maxLength);
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+function makeArrayTwo(firstArray, secondArray, maxLength) {
+  let resultArray = firstArray.concat(secondArray);
+  return resultArray.slice(0, maxLength);
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+console.log(makeArrayTwo(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArrayTwo(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArrayTwo(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArrayTwo(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArrayTwo(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(
+  makeArrayTwo(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)
+); // []
+
 console.log("end of task 2 ---");
--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,19 @@
 console.log("task 3");
-let content;
-let padding;
-let border;
 
-function getElementWidth(content, padding, border) {
-  const contentNumber = Number.parseFloat(content);
-  const paddingNumber = Number.parseFloat(padding);
-  const borderNumber = Number.parseFloat(border);
-  return contentNumber + paddingNumber * 2 + borderNumber * 2;
+function filterArray(numbers, value) {
+  const resultingArray = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      resultingArray.push(numbers[i]);
+    }
+  }
+  return resultingArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
 console.log("end of task 3 ---");
